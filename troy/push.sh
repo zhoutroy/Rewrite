0c521d87dc78f4cd94c7dd4948448627e2c25932
TroyZhou <azraelzxg@gmail.com>
2023-12-12 23:36:59 +0800

    test1

---
+++ troy/push.sh
@@ -0,0 +1,16 @@
+# 代码提交
+git push origin main
+
+# 文件转换路径
+# github原文件
+https://github.com/KOP-XIAO/QuantumultX/blob/master/Scripts/resource-parser.js
+# 官方raw文件
+https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
+# 加速后的raw文件（依然需要翻墙）
+https://ghproxy.com/https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
+
+# 合集资源路径
+https://raw.githubusercontent.com/zhoutroy/Script_X/main/Collections.conf
+
+# 重写资源路径
+https://raw.githubusercontent.com/zhoutroy/Rewrite/main/FT.js
